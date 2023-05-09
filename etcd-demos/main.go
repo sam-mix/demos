6a7fec8a882becedb6a18c9748f7067643eb8827
Sam Mix <1045262335@qq.com>
2023-05-09 14:05:48 +0800

    添加etcd demo

---
+++ etcd-demos/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"demos/etcd-demos/conn"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("conn.Client.Cluster", conn.Client.Cluster)
+	fmt.Println("conn.Client.Cluster", conn.Client.Cluster)
+	fmt.Println("conn.Client.Cluster", conn.Client.Cluster)
+}
