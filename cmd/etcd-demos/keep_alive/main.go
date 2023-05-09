7ee5e7109d8963267b419f874fd365a1af29284b
Sam Mix <1045262335@qq.com>
2023-05-09 14:51:15 +0800

    添加etcd demo

---
+++ cmd/etcd-demos/keep_alive/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
+)
+
+// etcd keepAlive
+
+func main() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: time.Second * 5,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connect to etcd success.")
+	defer cli.Close()
+
+	resp, err := cli.Grant(context.TODO(), 5)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = cli.Put(context.TODO(), "/nazha/", "dsb", clientv3.WithLease(resp.ID))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// the key 'foo' will be kept forever
+	ch, kaerr := cli.KeepAlive(context.TODO(), resp.ID)
+	if kaerr != nil {
+		log.Fatal(kaerr)
+	}
+	for {
+		ka := <-ch
+		fmt.Println("ttl:", ka.TTL)
+	}
+}
