e43fc64b29815c02d8b88f69532578fc154a28eb
Sam Mix <1045262335@qq.com>
2023-05-09 20:22:32 +0800

    添加dotnet demo

---
+++ cmd/big-int/main.go
@@ -0,0 +1,29 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"math/big"
+// )
+
+// func main() {
+// 	// 创建两个大整数
+// 	a := big.NewInt(12345678901234567890)
+// 	b := big.NewInt(98765432109876543210)
+
+// 	// 加法
+// 	c := new(big.Int).Add(a, b)
+// 	fmt.Println("a + b =", c)
+
+// 	// 乘法
+// 	d := new(big.Int).Mul(a, b)
+// 	fmt.Println("a * b =", d)
+
+// 	// 比较
+// 	if a.Cmp(b) < 0 {
+// 		fmt.Println("a < b")
+// 	} else if a.Cmp(b) > 0 {
+// 		fmt.Println("a > b")
+// 	} else {
+// 		fmt.Println("a = b")
+// 	}
+// }
---
+++ csharp/README.md
@@ -0,0 +1,11 @@
+dotnet new console -o sample1 是一个命令行命令，用于在当前目录下创建一个名为 sample1 的控制台应用程序。
+
+    dotnet 是 .NET CLI（Command Line Interface）的命令行工具。
+    new console 表示创建一个控制台应用程序。
+    -o sample1 表示指定输出目录为 sample1，即在当前目录下创建一个名为 sample1 的子目录，并将应用程序项目文件放置在该子目录中。
+
+因此，执行 dotnet new console -o sample1 命令后，会在当前目录下创建一个名为 sample1 的子目录，并在该子目录中创建一个控制台应用程序的项目文件和一些默认代码文件。这些默认代码文件包括 Program.cs，其中包含了一个名为 Main 的方法，它是控制台应用程序的入口点。
+ 
+```bash
+dotnet new console -o sample1
+```
